c3b26f0b4d9a884fd0890ea2e892e321c8ab2d41
Thiago Rosa <thiagorosadasilva39@gmail.com>
2025-05-11 21:04:45 -0300

    Delete src directory

--- src/api/auth.ts
+++
@@ -1,6 +0,0 @@
-import api from './index';
-
-export async function login(email: string, password: string) {
-  const response = await api.post('/auth/login', { username: email, password });
-  return response.data;
-}
